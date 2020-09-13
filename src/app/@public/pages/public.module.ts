1a9dc2bf9bfd29100ee5126855e7917769ea4c26
Dokhos <mendozaluca5@outlook.com>
2020-09-13 18:29:09 -0300

    Agregando los Compoenetes  en la parte Public

--- src/app/@public/pages/public.module.ts
+++ src/app/@public/pages/public.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
+import { PublicComponent } from './public.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PublicComponent],
   imports: [
     CommonModule,
     PublicRoutingModule
