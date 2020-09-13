1c9c93a3b207666f1a2887d8e80a2350add97c91
Dokhos <mendozaluca5@outlook.com>
2020-09-13 00:33:56 -0300

    Crado  ng g m @public/pages/home --routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MaterialModule } from './material.modules';
 
 @NgModule({
--- src/app/material.modules.ts
+++ src/app/material.modules.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 
-import { MatSliderModule } from '@angular/material/slider';
+import { } from '@angular/material/slider';
 
 
 
 @NgModule({
     imports: [
-        MatSliderModule
     ],
     exports: [
-        MatSliderModule
     ]
 })
 
