a6d8f5635fbcc8f9494ea045c3006d18ecf241da
Dokhos <mendozaluca5@outlook.com>
2020-09-15 21:20:37 -0300

    Añadimos en el header-admin @output- eventemitter

--- src/app/@admin/core/components/header/header.component.ts
+++ src/app/@admin/core/components/header/header.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-admin-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  // evaluando que la informacion empieze en true por defecto
+  toggledValue = true;
+  // enviando informacion al padre para ocultar la barra lateral del menu Admin
+  @Output() newItemEvent = new EventEmitter<boolean>();
 
-  ngOnInit(): void {
+  // la funcion toggles haces las evaluaciones de que estado esta el Buttom (True / False)
+  toggled() {
+    if (this.toggledValue === undefined) {
+      this.toggledValue = true;
+    }
+    this.toggledValue = !this.toggledValue;
+    this.newItemEvent.emit(this.toggledValue);
   }
 
 }
