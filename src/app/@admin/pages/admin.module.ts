e9c2c81e9c091d3bb1baec965d9ef4fcc8cb8aa3
Dokhos <mendozaluca5@outlook.com>
2020-10-07 21:01:29 -0300

    Configuración de los paht relativos para usar paht absolutos

--- src/app/@admin/pages/admin.module.ts
+++ src/app/@admin/pages/admin.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { TitleComponent } from '../core/components/title/title.component';
-import { HeaderComponent } from '../core/components/header/header.component';
-import { SidebarComponent } from '../core/components/sidebar/sidebar.component';
+
+import { TitleComponent } from '@admin-core/components/title/title.component';
+import { HeaderComponent } from '@admin-core/components/header/header.component';
+import { SidebarComponent } from '@admin-core/components/sidebar/sidebar.component';
 
 
 @NgModule({
