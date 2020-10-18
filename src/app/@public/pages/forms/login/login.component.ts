7b8c69c5a26d6deea5296f0863ebb4ee6a90c1d5
Dokhos <mendozaluca5@outlook.com>
2020-10-17 21:35:52 -0300

    Login - Se Crea HTML  Login y Style - Creamos Interfas   - Se le adapto  a Angular Forms

--- src/app/@public/pages/forms/login/login.component.ts
+++ src/app/@public/pages/forms/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ILoginForm } from '@core/interfaces/login.interface';
+import { AuthService } from '@core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  login: ILoginForm = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  init() {
+    console.log(this.login);
+
+  }
 }
