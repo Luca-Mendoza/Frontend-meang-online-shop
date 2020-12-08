b2caa883fcefc64cb2a439448c90dddb25113e8f
Dokhos <mendozaluca5@outlook.com>
2020-12-08 19:27:31 -0300

    Añadimos selector para seleccionar los items por página

--- src/app/@shared/table-pagination/table-pagination.module.ts
+++ src/app/@shared/table-pagination/table-pagination.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TablePaginationComponent } from './table-pagination.component';
@@ -10,7 +11,8 @@ import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
   declarations: [TablePaginationComponent],
   imports: [
     CommonModule,
-    NgbPaginationModule
+    NgbPaginationModule,
+    FormsModule
   ],
   exports: [
     TablePaginationComponent
