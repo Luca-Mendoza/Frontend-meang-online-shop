e61fee5cbc472b3e0cb72c84e03b7815372d65eb
Dokhos <mendozaluca5@outlook.com>
2020-12-15 16:17:18 -0300

    Más detalles - se agrega correctamente en los detalles de genero para poder 'editar' y 'bloquear' desde el mismo modal

--- src/app/@admin/pages/genres/genres.component.ts
+++ src/app/@admin/pages/genres/genres.component.ts
@@ -11,10 +11,9 @@ import { TYPE_ALERT } from '@shared/alerts/values.config';
 @Component({
   selector: 'app-genres',
   templateUrl: './genres.component.html',
-  styleUrls: ['./genres.component.scss']
+  styleUrls: ['./genres.component.scss'],
 })
 export class GenresComponent implements OnInit {
-
   // La consulta
   query: DocumentNode = GENRES_LIST_QUERY;
   // Información del contexto
@@ -28,29 +27,29 @@ export class GenresComponent implements OnInit {
   // definimos dato para hacer dinamica la Table
   columns: Array<ITableColumns>;
 
-  constructor(private service: GenresService) { }
+  constructor(private service: GenresService) {}
 
   ngOnInit(): void {
     this.context = {};
     this.itemsPage = 5;
     this.resultData = {
       listKey: 'genres',
-      definitionKey: 'genres'
+      definitionKey: 'genres',
     };
     this.include = false;
     this.columns = [
       {
         property: 'id',
-        label: '#'
+        label: '#',
       },
       {
         property: 'name',
-        label: 'Nombre del género'
+        label: 'Nombre del género',
       },
       {
         property: 'slug',
-        label: 'Slug'
-      }
+        label: 'Slug',
+      },
     ];
   }
 
@@ -62,14 +61,14 @@ export class GenresComponent implements OnInit {
     const genre = $event[1];
     console.log(genre);
 
+    let defaultValue = '';
+    if (genre.name !== undefined && genre.name !== '') {
+      defaultValue = genre.name;
+    }
+
+    const html = `<input id="name" value="${defaultValue}" class="swal2-input" required>`;
+
     if (action === 'edit' || action === 'add') {
-      let defaultValue = '';
-      if (genre.name !== undefined && genre.name !== '') {
-        defaultValue = genre.name;
-      }
-
-      const html = `<input id="name" value="${defaultValue}" class="swal2-input" required>`;
-
       if (action === 'add') {
         const result = await fromBasicDialog('Añadir género', html, 'name');
         console.log(result);
@@ -77,77 +76,92 @@ export class GenresComponent implements OnInit {
         return;
       }
       if (action === 'edit') {
-        const result = await fromBasicDialog('Modificar género', html, 'name');
-        console.log(result);
-        this.updateGenre(genre.id, result);
+        this.updateForm(html, genre);
         return;
       }
     } else {
       if (action === 'info') {
-        optionsWithDetails('Detalles', `${genre.name} (${genre.slug})`, 400,
-        '<i class="fas fa-edit"></i> Editar',
-        '<i class="fas fa-lock"></i> Block');
-        return;
-      }
-      if (action === 'block') {
-        const result = await optionsWithDetails('¿Bloquear?', 'Si bloqueas el item seleccionado, no se mostrará en la lista', 475,
-        'No, no bloquear',
-        'Si, bloquear');
-        if(!result) { // Si el resultado es falso, queremos bloquear
-          this.blockGenre(genre.id);
-          return;
+        const result = await optionsWithDetails(
+          'Detalles',
+          `${genre.name} (${genre.slug})`,
+          400,
+          '<i class="fas fa-edit"></i> Editar', // true
+          '<i class="fas fa-lock"></i> Block'
+        ); // false
+        if (result) {
+          this.updateForm(html, genre);
+        } else if (result === false) {
+          this.blockForm(genre);
         }
         return;
       }
+      if (action === 'block') {
+        this.blockForm(genre);
+
+        return;
+      }
     }
-
-
   }
 
   // tslint:disable-next-line: typedef
   addGenre(result) {
     if (result.value) {
-      this.service.addGenre(result.value).subscribe(
-        (res: any) => {
-          console.log(res);
-          if (res.status) {
-            basicAlert(TYPE_ALERT.SUCCESS, res.message);
-            return;
-          }
-          basicAlert(TYPE_ALERT.WARNING, res.message);
+      this.service.addGenre(result.value).subscribe((res: any) => {
+        console.log(res);
+        if (res.status) {
+          basicAlert(TYPE_ALERT.SUCCESS, res.message);
+          return;
         }
-      );
+        basicAlert(TYPE_ALERT.WARNING, res.message);
+      });
     }
   }
   // tslint:disable-next-line: typedef
   updateGenre(id: string, result) {
     console.log(id, result.value);
     if (result.value) {
-      this.service.update(id, result.value).subscribe(
-        (res: any) => {
-          console.log(res);
-          if (res.status) {
-            basicAlert(TYPE_ALERT.SUCCESS, res.message);
-            return;
-          }
-          basicAlert(TYPE_ALERT.WARNING, res.message);
+      this.service.update(id, result.value).subscribe((res: any) => {
+        console.log(res);
+        if (res.status) {
+          basicAlert(TYPE_ALERT.SUCCESS, res.message);
+          return;
         }
-      );
+        basicAlert(TYPE_ALERT.WARNING, res.message);
+      });
     }
   }
 
-    // tslint:disable-next-line: typedef
-    blockGenre(id: string) {
-      this.service.block(id).subscribe(
-        (res: any) => {
-          console.log(res);
-          if (res.status) {
-            basicAlert(TYPE_ALERT.SUCCESS, res.message);
-            return;
-          }
-          basicAlert(TYPE_ALERT.WARNING, res.message);
-        }
-      );
-    }
+  // tslint:disable-next-line:typedef
+  async updateForm(html: string, genre: any) {
+    const result = await fromBasicDialog('Modificar género', html, 'name');
+    console.log(result);
+    this.updateGenre(genre.id, result);
+  }
 
+  // tslint:disable-next-line: typedef
+  blockGenre(id: string) {
+    this.service.block(id).subscribe((res: any) => {
+      console.log(res);
+      if (res.status) {
+        basicAlert(TYPE_ALERT.SUCCESS, res.message);
+        return;
+      }
+      basicAlert(TYPE_ALERT.WARNING, res.message);
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  async blockForm(genre: any) {
+    const result = await optionsWithDetails(
+      '¿Bloquear?',
+      'Si bloqueas el item seleccionado, no se mostrará en la lista',
+      475,
+      'No, no bloquear',
+      'Si, bloquear'
+    );
+    if (result === false) {
+      // Si el resultado es falso, queremos bloquear
+      this.blockGenre(genre.id);
+    }
+  }
 }
