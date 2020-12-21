6b98e8194c78a97a059a6018cd54af740a491c73
Dokhos <mendozaluca5@outlook.com>
2020-12-21 15:28:28 -0300

    Añadimoslas funcionalidades agregar-tag/editar-tag/bloquear-tag /=> tag.service.ts

--- src/app/@admin/pages/tags/tags.service.ts
+++ src/app/@admin/pages/tags/tags.service.ts
@@ -1,9 +1,48 @@
+import { ADD_TAG, BLOCK_TAG, MODIFY_TAG } from './../../../@graphql/operations/mutation/tag';
+import { Apollo } from 'apollo-angular';
+import { ApiService } from './../../../@graphql/services/api.service';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/internal/operators/map';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TagsService {
+export class TagsService extends ApiService {
 
-  constructor() { }
+  constructor(apollo: Apollo) {
+    super(apollo);
+  }
+
+  addTag(name: string) {
+    return this.set(
+      ADD_TAG,
+      {
+        genre: name
+      }, {}).pipe(map((result: any) => {
+        return result.addTag;
+      }));
+  }
+
+  update(id: string, name: string) {
+    return this.set(
+      MODIFY_TAG,
+      {
+        id,
+        tag: name
+      }, {}).pipe(map((result: any) => {
+        return result.updateTag;
+      }));
+  }
+
+  block(id: string) {
+    return this.set(
+      BLOCK_TAG,
+      {
+        id
+      }, {}).pipe(map((result: any) => {
+        return result.blockTag;
+      }));
+  }
+
+
 }
