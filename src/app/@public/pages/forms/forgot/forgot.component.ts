a7fef62fd18b845008994745314c9b22ebbc0f74
Dokhos <mendozaluca5@outlook.com>
2021-01-20 14:53:01 -0300

    Resetar password - Añadimos los modales de Alert para informar al usuario si existe o no

--- src/app/@public/pages/forms/forgot/forgot.component.ts
+++ src/app/@public/pages/forms/forgot/forgot.component.ts
@@ -1,6 +1,8 @@
+import { basicAlert } from '@shared/alerts/toasts';
 import { Component, OnInit, ɵConsole } from '@angular/core';
 import { EMAIL_PATTERN } from '@core/constants/regex';
 import { PasswordService } from '@core/services/password.service';
+import { TYPE_ALERT } from '@shared/alerts/values.config';
 
 @Component({
   selector: 'app-forgot',
@@ -9,17 +11,22 @@ import { PasswordService } from '@core/services/password.service';
 })
 export class ForgotComponent implements OnInit {
   emailValue: string;
-  pattern =  EMAIL_PATTERN;
+  pattern = EMAIL_PATTERN;
 
-  constructor( private passwordService: PasswordService) { }
+  constructor(private passwordService: PasswordService) { }
 
   ngOnInit(): void {
   }
 
-  reset(){
+  reset() {
     console.log('reseteando');
     this.passwordService.reset(this.emailValue).subscribe(result => {
       console.log(result);
+      if (result.status) {
+        basicAlert(TYPE_ALERT.SUCCESS, result.message);
+        return;
+      }
+      basicAlert(TYPE_ALERT.WARNING, result.message);
     });
   }
 
