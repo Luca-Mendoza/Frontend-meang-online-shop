487532f2b843fd60334700c7c4d5cc3a909a7c3f
Dokhos <mendozaluca5@outlook.com>
2021-03-17 20:44:13 -0300

    Aplicando en el Carousel dinámico con últimos productos en stock

--- src/app/@public/pages/home/home.component.ts
+++ src/app/@public/pages/home/home.component.ts
@@ -1,9 +1,8 @@
+import { IProduct } from '@mugan86/ng-shop-ui/lib/interfaces/product.interface';
 import { ProductsService } from '@core/services/products.service';
 import { Component, OnInit } from '@angular/core';
-import { UsersService } from '@core/services/users.service';
 import { ICarouselItem } from '@mugan86/ng-shop-ui/lib/interfaces/carousel-item.interface';
 import carouselItem from '@data/carousel.json';
-import productsList from '@data/products.json';
 import { ACTIVE_FILTERS } from '@core/constants/filter';
 
 @Component({
@@ -17,46 +16,41 @@ export class HomeComponent implements OnInit {
   listOne;
   listTwo;
   listThree;
-  constructor(
-    private usersApi: UsersService,
 
-    private product: ProductsService
-  ) {}
+  constructor(private product: ProductsService) {}
 
   ngOnInit(): void {
-    this.product
-      .getByLastUnitsOffers(1, 4, ACTIVE_FILTERS.ACTIVE, true, 35, 40)
-      .subscribe((result) => {
-        console.log('productos menos de 35', result);
-        this.listTwo = result;
-      });
+    // productos PC
     this.product
       .shopProductsPlatforms(1, 4, ACTIVE_FILTERS.ACTIVE, true, '4')
       .subscribe((result) => {
-        console.log('productos por plataforma', result);
         this.listOne = result;
       });
-    this.items = carouselItem;
-    this.productsList = productsList;
-
-    // console.log('Carousel items', this.items);
-    // tslint:disable-next-line: deprecation
-    this.usersApi.getUsers(2, 1).subscribe((result) => {
-      // console.log(result); // { {obtener la Info : status message users: []}
-    });
-    this.listOne = this.fakeRandomProductsList();
-    this.listTwo = this.fakeRandomProductsList();
-    this.listThree = this.fakeRandomProductsList();
-  }
-
-  fakeRandomProductsList() {
-    const list = [];
-    const arrayMax = 4;
-    const limit = this.productsList.length;
-
-    for (let i = 0; i < arrayMax; i++) {
-      list.push(this.productsList[Math.floor(Math.random() * limit)]);
-    }
-    return list;
+    // Producto mas economicos $35 y que tengamos en stock -40
+    this.product
+      .getByLastUnitsOffers(1, 4, ACTIVE_FILTERS.ACTIVE, true, 35, 40)
+      .subscribe((result) => {
+        this.listTwo = result;
+      });
+    // productos ps4
+    this.product
+      .shopProductsPlatforms(1, 4, ACTIVE_FILTERS.ACTIVE, true, '18')
+      .subscribe((result) => {
+        this.listThree = result;
+      });
+    // Producto que van en el carousel
+    this.product
+      .getByLastUnitsOffers(1, 4, ACTIVE_FILTERS.ACTIVE, true, -1, 20)
+      .subscribe((result: IProduct[]) => {
+        result.map((item: IProduct) => {
+          this.items.push({
+            id: item.id,
+            title: item.name,
+            description: item.description,
+            background: item.img,
+            url: '',
+          });
+        });
+      });
   }
 }
