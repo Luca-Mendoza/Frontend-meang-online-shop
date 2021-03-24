97bed68eb1eb70548a40045f1aa8064b0d232d99
Dokhos <mendozaluca5@outlook.com>
2021-03-24 16:16:15 -0300

    Añadida la información de los productos principal

--- src/app/@public/pages/games/details/details.component.ts
+++ src/app/@public/pages/games/details/details.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import products from '@data/products.json';
+import { CURRENCIES_SYMBOL, CURRENCY_LIST } from '@mugan86/ng-shop-ui';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  // Función  aleatoria para que recorra el total de la lista
+  products = products[Math.floor(Math.random() * products.length)];
+  selectImage = this.products.img;
+  corruncySelect = CURRENCIES_SYMBOL[CURRENCY_LIST.US_DOLLAR];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
