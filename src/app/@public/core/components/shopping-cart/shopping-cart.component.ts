727a68d21d7dbd076006de620d8db7eab772a751
Dokhos <mendozaluca5@outlook.com>
2021-04-01 16:05:34 -0300

    Crear menú lateral para el carrito de compra

--- src/app/@public/core/components/shopping-cart/shopping-cart.component.ts
+++ src/app/@public/core/components/shopping-cart/shopping-cart.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.scss']
+  styleUrls: ['./shopping-cart.component.scss'],
 })
 export class ShoppingCartComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openNav() {
+    console.log('openNav');
+    document.getElementById('mySidenav').style.width = '250px';
   }
 
+  closeNav() {
+    console.log('closeNav');
+    document.getElementById('mySidenav').style.width = '0';
+  }
 }
