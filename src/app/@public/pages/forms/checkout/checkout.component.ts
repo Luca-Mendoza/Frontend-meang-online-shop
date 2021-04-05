e9b18191d980185148981ee43b90891b02f1ba48
Dokhos <mendozaluca5@outlook.com>
2021-04-05 17:19:06 -0300

    Añadir información de autenticación y mostrarla. Controlar el acceso al formulario de pago, solo se ingresamos si estamos logueado

--- src/app/@public/pages/forms/checkout/checkout.component.ts
+++ src/app/@public/pages/forms/checkout/checkout.component.ts
@@ -1,15 +1,27 @@
+import { Router } from '@angular/router';
+import { AuthService } from '@core/services/auth.service';
+import { IMeData } from '@core/interfaces/session.interface';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  meData: IMeData;
+  constructor(private auth: AuthService, private route: Router) {
+    this.auth.accessVar$.subscribe((data: IMeData) => {
+      if (!data.status) {
+        // Ir al login
+        this.route.navigate(['/login']);
+        return;
+      }
+      this.meData = data;
+    });
   }
 
+  ngOnInit(): void {
+    this.auth.start();
+  }
 }
