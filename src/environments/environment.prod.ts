0b5a36601d06a0ca0a80de6f4788e2f7264b8a2b
Dokhos <mendozaluca5@outlook.com>
2021-11-25 21:45:54 -0300

    Backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  backend: 'https://gamezonia-shop.herokuapp.com/graphql',
   stripePublicKey:
     'pk_test_51IdFgILcAMPJSB0NM7ohOaQWM1VlLPmxyKxi81Ki7kgXEKBzklYD0jQ7Mr7sR464DU3Tdco4CRLofVPSi7k9XF4y00Gdy8kNoI',
 };
