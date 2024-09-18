7efc4e38868ce2eb0a2cfbdfc0da28ed2db9ac72
Luca-Mendoza-Coder <100142127+Luca-Mendoza-Coder@users.noreply.github.com>
2024-09-18 01:09:33 -0300

    fixer - url serve

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  backend: 'https://gamezonia-shop.herokuapp.com/graphql',
-  backendWs: 'wss://gamezonia-shop.herokuapp.com/graphql',
+  backend: 'https://backend-meang-online-shop.vercel.app/graphql', // Ruta correcta para las peticiones POST
+  backendWs: 'wss://backend-meang-online-shop.vercel.app/graphql', // Si usas WebSocket
 
   stripePublicKey:
     'pk_test_51IdFgILcAMPJSB0NM7ohOaQWM1VlLPmxyKxi81Ki7kgXEKBzklYD0jQ7Mr7sR464DU3Tdco4CRLofVPSi7k9XF4y00Gdy8kNoI',
