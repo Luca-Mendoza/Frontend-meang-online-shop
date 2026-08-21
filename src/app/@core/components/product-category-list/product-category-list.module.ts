52c09dfe4af35355b91193d40f00958f15035ab6
Luca Mendoza <mendozaluca5@outlook.com>
2026-08-21 19:18:30 -0300

    fix: import RouterModule in ProductCategoryListModule for routerLink support

--- src/app/@core/components/product-category-list/product-category-list.module.ts
+++ src/app/@core/components/product-category-list/product-category-list.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ProductCategoryListComponent } from './product-category-list.component';
 import { ProductItemModule } from '@mugan86/ng-shop-ui';
 
 @NgModule({
   declarations: [ProductCategoryListComponent],
-  imports: [CommonModule,
-            ProductItemModule],
+  imports: [CommonModule, RouterModule, ProductItemModule],
   exports: [ProductCategoryListComponent],
 })
 export class ProductCategoryListModule { }
